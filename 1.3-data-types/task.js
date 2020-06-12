4bd660a0379a7a0a0e527b5d6f3f21dc3927a73f
LiquidAssContainer <LiquidAssContainer@users.noreply.github.com>
2020-06-13 00:07:01 +0500

    Третья домашняя работа

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,41 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (isNaN(percent)) {
+        return `Параметр «${window.percent.placeholder}» содержит неправильное значение ${window.percent.value}`;
+    }
+
+    if (isNaN(contribution)) {
+        return `Параметр «${window.contribution.placeholder}» содержит неправильное значение ${window.contribution.value}`;
+    }
+
+    if (isNaN(amount)) {
+        return `Параметр «${window.amount.placeholder}» содержит неправильное значение ${window.amount.value}`;
+    }
+
+    let currentDate = new Date();
+
+    if (isNaN(date.valueOf()) || (currentDate >= date)) {
+        return 'Неверно указана дата';
+    }
+
+    let remainingAmount = amount - contribution,
+        percentage = percent / 100 / 12,
+        monthsQuantity = (date.getFullYear() - currentDate.getFullYear()) * 12;
+
+    let monthSum = remainingAmount * (percentage + percentage / (((1 + percentage) ** monthsQuantity) - 1));
+    let totalSum = monthSum * monthsQuantity;
+    totalSum = Math.round(totalSum * 100) / 100;
+
+    console.log(totalSum);
+    return totalSum;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (!name) {
+        name = 'Аноним';
+    }
+
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    return greeting;
 }
