989c556738be4c65f4031c8dfde689e344b401bf
LiquidAssContainer <LiquidAssContainer@users.noreply.github.com>
2020-06-25 13:44:59 +0500

    2.2-object-prototype done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,53 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function(string) {
+    if (string === '') {
+        return false;
+    }
+    
+    string = this.toLowerCase();
+    console.log(string)
+
+    string = string.split(' ').join('');
+    console.log(string)
+
+    let reversedString = string.split("").reverse().join("");
+    console.log(reversedString)
+    
+    if (string === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+
+    let averageMark = sum ? (sum / marks.length) : 0;
+    let roundedAverage = Math.round(averageMark);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date(),
+        verdict;
+
+        console.log(birthday)
+    birthday = Date.parse(birthday);
+    console.log(birthday)
+    console.log(typeof now)
+    console.log(typeof birthday)
+    // birthday = birthday.getTime();
+    let diff = +now - +birthday;
+    console.log(diff)
+    if ((diff / 1000 / 3600 / 24 / 365) >= 18) {
+        verdict = true;
+    } else {
+        verdict = false;
+    }
+
+    return verdict
 }
